65d67b79bd0af4e102a0eead59a48a84c99db620
Ivan Pinheiro <Ivanppf1@gmail.com>
2024-04-08 13:19:52 -0300

    FIX added repository annotation

--- src/main/java/br/com/compassuol/sp/challenge/msproducts/model/repository/CategoryRepository.java
+++ src/main/java/br/com/compassuol/sp/challenge/msproducts/model/repository/CategoryRepository.java
@@ -2,8 +2,10 @@ package br.com.compassuol.sp.challenge.msproducts.model.repository;
 
 import br.com.compassuol.sp.challenge.msproducts.model.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
 }
